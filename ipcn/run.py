6fe99152b55e3646322581947e1a30e6e9cb3fb6
plusplus1 <comeson@126.com>
2020-06-30 22:52:44 +0800

    fix

--- ipcn/run.py
+++ ipcn/run.py
@@ -9,9 +9,9 @@ def main():
 
     for ip_arg in sys.argv[1:]:
         try:
-            print("%s\t\t%s", (ip_arg, query_ip_location(ip_arg)))
+            print("%s\t\t%s" % (ip_arg, query_ip_location(ip_arg)))
         except Exception as err:
-            print("%s\t\tERROR:%s", (ip_arg, err))
+            print("%s\t\tERROR:%s" % (ip_arg, err))
             pass
 
 
